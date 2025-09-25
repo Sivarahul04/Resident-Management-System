36ea2e26f1aac4c2fdad94034663eac4cc3d3e7e
Siva <sivarahul56@gmail.com>
2025-09-25 10:02:00 +0530

    Source Code

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from './Pages/Login';
+import Newuser from "./Pages/Newuser";
+import Forgot from "./Pages/Forgot";
+import UserDashboard from "./Pages/UserDashboard";
+import ViewRequest from "./Pages/ViewRequest";
+import RaiseRequest from "./Pages/RaiseRequest";
+import AdminDashboard from "./Pages/AdminDashboard";
+import ManageUsers from "./Pages/ManageUsers";
+import ManageServices from "./Pages/ManageServices";
+import ManageRequest from "./Pages/ManageRequest";
+import SuperAdminDashboard from "./Pages/SuperAdminDashboard";
+import SuperAdminReports from "./Pages/SuperAdminReports";
+import Apis from "./Components/Apis";
+import EmployeeApi from "./Components/EmployeeApi";
+import Input from "./Components/Input";
+import EmployeeInput from "./Components/EmployeeInput";
+import RequestDetail from "./Pages/RequestDetail";
+import Users from "./Components/Users";
+// import EditForm from "./Components/EditForm";
+import EditRequest from "./Pages/EditRequest";
+import RequestsPage from "./Pages/RequestsPage";
+import ManageAdmin from "./Pages/ManageAdmin";
+// import ToDo from "./Rough/ToDo";
+// import About from "./Components/About";
+import Rough from "./Components/Rough";
+import Simply from "./Components/Simply";
+
+
+function App() {
+  
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/newuser" element={<Newuser />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        <Route path="/raiserequest" element={<RaiseRequest  />} />
+        <Route path="/viewrequest" element={<ViewRequest  />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/manageusers" element={<ManageUsers />} />
+        <Route path="/manageservices" element={<ManageServices />} />
+        <Route path="/managerequests" element={<ManageRequest />} />
+        <Route path="/superadmindashboard" element={<SuperAdminDashboard />} />
+        <Route path="/reports" element={<SuperAdminReports />} />
+        <Route path="/requestdetail/:id" element={<RequestDetail />}/>
+        <Route path="/editrequest/:id" element={<EditRequest />}/>
+        <Route path="/requests" element={<RequestsPage />  } />
+        <Route path="/admin" element={<ManageAdmin />}/>
+       
+
+
+        <Route path="/apis" element={<Apis />}/>
+        <Route path="/table" element={<EmployeeApi />} />
+        <Route path="/input" element={<Input />}/>
+        <Route path="/employee" element={<EmployeeInput />} />
+        <Route path="/users" element={<Users />}/>
+        <Route path="/edit" element={<Rough/>}/>
+        <Route path="/todo" element={<Simply />}/>
+
+
+        
+ 
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
